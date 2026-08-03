bab7a4ce7d71079cae852a51d1eb300391c0ddb3
KirisameMarishark <steinsgatelabmemnumber001@gmail.com>
2026-08-03 09:15:24 +0800

    feat(reality): generate default config

--- internal/reality/generate.sh
+++ internal/reality/generate.sh
@@ -12,3 +12,15 @@ reality_generate_keypair() {
 reality_generate_shortid() {
     openssl rand -hex 8
 }
+
+reality_private_key() {
+    xray x25519 | awk -F': ' '/^PrivateKey:/ {print $2}'
+}
+
+reality_public_key() {
+    xray x25519 | awk -F': ' '/^Password \(PublicKey\):/ {print $2}'
+}
+
+reality_hash() {
+    xray x25519 | awk -F': ' '/^Hash32:/ {print $2}'
+}
