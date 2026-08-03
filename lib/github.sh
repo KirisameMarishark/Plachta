a9f1fc67c30d437eb0503d5ebea5d1dbe6db3601
KirisameMarishark <steinsgatelabmemnumber001@gmail.com>
2026-08-03 08:42:35 +0800

    fix(github): use GitHub API headers

--- lib/github.sh
+++ lib/github.sh
@@ -4,6 +4,9 @@ github_latest_release() {
 
     local repo="$1"
 
-    curl -fsSL "https://api.github.com/repos/${repo}/releases/latest"
+    curl -fsSL \
+        -H "Accept: application/vnd.github+json" \
+        -H "X-GitHub-Api-Version: 2022-11-28" \
+        "https://api.github.com/repos/${repo}/releases/latest"
 
 }
